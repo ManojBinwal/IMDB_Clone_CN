f14e1e2c31b29620666a33bf4fdff8f2d01e456c
Manoj Kumar Binwal <114239126+ManojBinwal@users.noreply.github.com>
2023-08-02 14:24:31 +0530

    final touches

--- script.js
+++ script.js
@@ -1,78 +1,85 @@
+// API endpoints and key for The Movie Database API
 const apiBaseUrl = "https://api.themoviedb.org/3/";
 const apiKey = "d6bd8af122f2b68bc1dee55e05623e10";
+
+// Base URL for movie poster images
 const imageBaseUrl = "https://image.tmdb.org/t/p/w300";
 
-const moviesGrid = document.getElementById("movies-grid");
+// DOM elements
+const moviesGrid = document.getElementById("movies-grid"); // Container for displaying movie cards
+const searchInput = document.getElementById("search-input"); // Input field for search queries
+const searchForm = document.getElementById("search-form"); // Search form element
+const categoryTitle = document.getElementById("category-title"); // Heading for movie category titles
 
-const searchInput = document.getElementById("search-input");  // variable to hold query //
-const searchForm = document.getElementById("search-form"); 
-const categoryTitle = document.getElementById("category-title");
-
-//create an async function to perform API call //
+// Fetch movies currently playing in theaters
 async function fetchMoviesNowPlaying() {
   const response = await fetch(
     `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`
-  ); 
+  );
   const jsonResponse = await response.json();
-  const movies = await Promise.all (
+  const movies = await Promise.all(
     jsonResponse.results.map(async (movie) => ({
-        id: movie.id,
-        title: movie.title,
-        poster_path: movie.poster_path,
-        vote_average: movie,
-        IMDbId: await getIMDbId(movie.id)
-  }))
-  )
+      id: movie.id,
+      title: movie.title,
+      poster_path: movie.poster_path,
+      vote_average: movie.vote_average,
+      IMDbId: await getIMDbId(movie.id),
+    }))
+  );
   displayMovies(movies);
 }
 
- // create function to fetch movie poster using base url and movie poster path //
+// Display movies on the webpage
 function displayMovies(movies) {
   moviesGrid.innerHTML = movies
     .map(
-      (movie) =>
-        ` <div class = "movie-card">
-        <a href = "https://www.imdb.com/title/${movie.IMDbId}/"> 
-        <img src = "${imageBaseUrl}${movie.poster_path}"/>
-        <p>⭐${movie.vote_average}</p>
-        <h1>${movie.title}</h1>
-        </div>
-        
-        `
+      (movie) => `
+      <div class="movie-card">
+        <a href="https://www.imdb.com/title/${movie.IMDbId}/"> 
+          <img src="${imageBaseUrl}${movie.poster_path}" />
+          <p>⭐ ${movie.vote_average}</p>
+          <h1>${movie.title}</h1>
+        </a>
+      </div>`
     )
-    .join(
-      ""
-    ); // use join to convert the array of string derived from map into a single long string to use with inner HTML //
+    .join(""); // Convert the array of strings derived from the map into a single string to use with innerHTML
 }
 
-//function to handle search form on submit //
+// Handle search form submission
 function handleSearchFormSubmit(event) {
-    event.preventDefault();
-    categoryTitle.innerHTML = "Search Results"
-    const searchQuery = searchInput.value;
-    const movies = searchMovies(searchQuery);
-    displayMovies(movies);
+  event.preventDefault();
+  categoryTitle.innerHTML = "Search Results";
+  const searchQuery = searchInput.value;
+  searchMovies(searchQuery);
 }
 
-
-
+// Search movies based on the given query
 async function searchMovies(query) {
-    const response = await fetch(`${apiBaseUrl}/search/movie?api_key=${apiKey}&query="${query}"`);
-    const jsonResponse = await response.json();
-    const movies = jsonResponse.results;
-    displayMovies(movies);
+  const response = await fetch(
+    `${apiBaseUrl}/search/movie?api_key=${apiKey}&query="${query}"`
+  );
+  const jsonResponse = await response.json();
+  const movies = jsonResponse.results;
+  displayMovies(movies);
 }
 
-async function getIMDbId(movieId){
-    const response = await fetch(`${apiBaseUrl}/movie/${movieId}/external_ids?api_key=${apiKey}`);
-    const jsonResponse = await response.json();
-    return jsonResponse.imdb_id;
-
+// Fetch the IMDb ID of a movie based on its TMDB ID
+async function getIMDbId(movieId) {
+  const response = await fetch(
+    `${apiBaseUrl}/movie/${movieId}/external_ids?api_key=${apiKey}`
+  );
+  const jsonResponse = await response.json();
+  return jsonResponse.imdb_id;
 }
 
+// Search movies with the query "Batman" (you can replace this with any other query)
 searchMovies("Batman");
 
+// Add event listener to handle form submissions
 searchForm.addEventListener("submit", handleSearchFormSubmit);
-fetchMoviesNowPlaying(); 
 
-// console.log(getIMDbId(550))
+// Fetch and display movies currently playing in theaters
+fetchMoviesNowPlaying();
+
+// Example of getting the IMDb ID for the movie with TMDB ID 550 (commented out since it's not currently in use)
+// console.log(getIMDbId(550));
